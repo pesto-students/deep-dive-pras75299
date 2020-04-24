f8bc99a51a667e6db0100aa7d09bb6c22a3a74f0
prashant kumar singh <singh.prashantking@gmail.com>
2020-04-25 01:16:09 +0530

    solve bouncer question

---
+++ js-exercises/bouncer/bouncer.js
@@ -0,0 +1,5 @@
+function bouncer(array) {
+  return array.filter(Boolean);
+}
+
+export { bouncer };
---
+++ js-exercises/bouncer/bouncer.test.js
@@ -0,0 +1,13 @@
+import { bouncer } from "./bouncer";
+
+describe("bouncer", () => {
+  test("should return a object", () => {
+    expect(typeof bouncer(["a", "b", "c"])).toEqual("object");
+  });
+  test("should remove all falsy values from an array.", () => {
+    expect(bouncer([7, "ate", "", false, 9])).toEqual([7, "ate", 9]);
+    expect(bouncer(["a", "b", "c"])).toEqual(["a", "b", "c"]);
+    expect(bouncer([false, null, 0, NaN, undefined, ""])).toEqual([]);
+    expect(bouncer([1, null, NaN, 2, undefined])).toEqual([1, 2]);
+  });
+});
